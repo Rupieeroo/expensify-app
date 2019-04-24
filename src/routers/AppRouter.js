529084e9400a63f871814273300c68a37a361c73
Rupert Adams <RupertNCAdams@aol.com>
2019-04-24 16:51:31 +0100

    moved out components and imported them into router

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,45 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
-
-const ExpenseDashboardPage = () => (
-  <div>
-    This is the Dashboard Component
-  </div>
-);
-
-const AddExpensePage = () => (
-  <div>
-    This is the Add Expense Component
-  </div>
-);
-
-const EditExpensePage = () => (
-  <div>
-    This is the Edit Expense Component
-  </div>
-);
-
-const HelpPage = () => (
-  <div>
-    This is the Help Page Component
-  </div>
-);
-
-const NotFoundPage = () => (
-  <div>
-    404 - <Link to="/">Go Home</Link>
-  </div>
-);
-
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <NavLink to="/edit" activeClassName="is-active">Edit</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-  </header>
-);
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import AddExpensePage from '../components/AddExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
+import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
+import Header from '../components/Header';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const AppRouter = () => (
   <BrowserRouter>
