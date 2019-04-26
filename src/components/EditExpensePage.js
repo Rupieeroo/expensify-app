609fccefa5bed01148a5af83d4364ed169a1f585
Rupert Adams <RupertNCAdams@aol.com>
2019-04-26 15:56:21 +0100

    changed the component to allow props

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-  <div>
-    This is the Edit Expense Component
-  </div>
-);
+const EditExpensePage = (props) => {
+
+  console.log(props);
+
+  return (
+    <div>
+      Editing the expense with an id of {props.match.params.id}
+    </div>
+  );
+};
+
 
 export default EditExpensePage;
