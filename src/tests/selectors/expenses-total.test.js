b5b936a9548c822eea324bfaab1ed03ea30818e7
Rupert Adams <RupertNCAdams@aol.com>
2019-06-17 16:42:04 +0100

    added tests for expenses total on single and multiple expenses

--- src/tests/selectors/expenses-total.test.js
+++ src/tests/selectors/expenses-total.test.js
@@ -6,3 +6,15 @@ test('should return 0 if no expenses', () => {
 
   expect(res).toBe(0);
 });
+
+test('should correctly add up a single expense', () => {
+  const res = selectExpensesTotal([expenses[0]]);
+
+  expect(res).toBe(195);
+});
+
+test('should correctly add up multiple expenses', () => {
+  const res = selectExpensesTotal([expenses]);
+
+  expect(res).toBe(114195);
+});
