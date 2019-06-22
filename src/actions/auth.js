7a422c7556fa3f09c162b973190f9e9f2f52a831
Rupert Adams <RupertNCAdams@aol.com>
2019-06-22 20:18:09 +0100

    added auth reducer and relevent tests

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,10 +1,19 @@
 import { firebase, googleAuthProvider } from '../firebase/firebase';
 
+export const logIn = (uid) => ({
+  type: 'LOGIN',
+  uid: uid
+});
+
 export const startLogIn = () => {
   return () => {
     return firebase.auth().signInWithPopup(googleAuthProvider);
   };
 };
+
+export const logOut = () => ({
+  type: 'LOGOUT'
+});
 
 export const startLogOut = () => {
   return () => {
