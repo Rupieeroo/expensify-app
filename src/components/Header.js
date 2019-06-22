80b3b23fb9b2c9fce20b65108bba0771fb6d5161
Rupert Adams <RupertNCAdams@aol.com>
2019-06-22 19:09:09 +0100

    added a log out button to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { startLogOut } from '../actions/auth';
 
-const Header = () => (
+export const Header = ({ startLogOut }) => (
   <header>
     <h1>Expensify</h1>
     <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
     <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
     <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    <button onClick={startLogOut}>Log Out</button>
   </header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispatch) =>({
+  startLogOut: () => dispatch(startLogOut())
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
